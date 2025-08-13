ccb14caab8a2d1af20c7afb37744dbbee3c40790
Eden Zimbelman <eden.zimbelman@salesforce.com>
2025-08-12 23:31:02 -0700

    feat: introduce the blank template (#1)
    Co-authored-by: Michael Brooks <michael@michaelbrooks.ca>

---
+++ app.js
@@ -0,0 +1,15 @@
+const { App } = require('@slack/bolt');
+
+// Initializes your app with your Slack app and bot token
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+});
+
+(async () => {
+  // Start your app
+  await app.start();
+
+  app.logger.info('⚡️ Bolt app is running!');
+})();
